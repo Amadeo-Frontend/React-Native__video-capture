b1b607301ff5bc3b2f6df096a3a7805674a7ae05
Amadeo Bon <amadeosph@outlook.com>
2024-02-04 00:56:34 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Camera } from "expo-camera";
+import { Audio, Video } from "expo-av";
+import * as MediaLibrary from "expo-media-library";
+import { shareAsync } from "expo-sharing";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ docs.md
@@ -0,0 +1,23 @@
+# Camera
+
+npx expo install expo-camera
+
+https://docs.expo.dev/versions/latest/sdk/camera/
+
+# Video e audio
+
+npx expo install expo-av
+
+https://docs.expo.dev/versions/latest/sdk/av/
+
+# Albuns
+
+npx expo install expo-media-library
+
+https://docs.expo.dev/versions/latest/sdk/media-library/
+
+# Sharing
+
+npx expo install expo-sharing
+
+https://docs.expo.dev/versions/latest/sdk/sharing/
